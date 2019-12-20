2cf93f98334a936673167157efb037eab0ddc271
tsochetra <than.sochetra@icloud.com>
2019-12-20 12:43:33 +0700

    finished task 1

--- src/tp_note_1/Sample.java
+++
@@ -1,7 +0,0 @@
-package tp_note_1;
-
-public class Sample {
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
-	}
-}
---
+++ src/tp_note_1/Task1.java
@@ -0,0 +1,16 @@
+package tp_note_1;
+
+public class Task1 {
+	public static void main(String[] args) {
+		System.out.println("**************************");
+		System.out.println("*                      * *");
+		System.out.println("*                   *    *");
+		System.out.println("*                *       *");
+		System.out.println("*             *          *");
+		System.out.println("*          *             *");
+		System.out.println("*       *                *");
+		System.out.println("*    *                   *");
+		System.out.println("* *                      *");
+		System.out.println("**************************");
+	}
+}
